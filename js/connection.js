2ab1051c552a80db28af6c97ad9e132198d3a971
SkaveRat <skaverat@funlynx.de>
2014-12-20 19:06:54 +0100

    receive game size

--- js/connection.js
+++ js/connection.js
@@ -1,25 +1,28 @@
 $(document).ready(function(){
-    var socket = new WebSocket("ws://localhost:8080/echo");
-
+    var socket = new WebSocket("ws://localhost:8080/state");
     var gridSize = 30;
-    var boardX = 20;
-    var boardY = 10;
-    var boardXpx = gridSize * boardX;
-    var boardYpx = gridSize * boardY;
+
+    $.get('http://localhost:8080/gamedata', function(data) {
+        data = JSON.parse(data);
+        var boardX = data.size[0];
+        var boardY = data.size[1];
+        var boardXpx = gridSize * boardX;
+        var boardYpx = gridSize * boardY;
+        $('#board').css('height', boardYpx);
+        $('#board').css('width', boardXpx);
+    });
 
     var ballSize = gridSize;
 
     $('#ball').css('height', ballSize);
     $('#ball').css('width', ballSize);
+    $('#ball').css('display', 'block');
 
-    $('#board').css('height', boardYpx);
-    $('#board').css('width', boardXpx);
 
     socket.onmessage = function(res) {
         var data = JSON.parse(res.data);
-        var posX = data.position[0];
-        var posY = data.position[1];
-        console.log(data);
+        var posX = data.b[0];
+        var posY = data.b[1];
         $('#ball').css('left', posX * gridSize);
         $('#ball').css('top', posY * gridSize);
     };
